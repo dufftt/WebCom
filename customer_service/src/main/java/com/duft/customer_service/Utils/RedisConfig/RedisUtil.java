dd605975e53a1d54e296b1ef931f57cbb11c99bf
Sourav <rockythere81@gmail.com>
2026-03-24 01:07:40 +0530

    added common maven module for proto contracts and graphql schemas

--- customer_service/src/main/java/com/duft/customer_service/Utils/RedisConfig/RedisUtil.java
+++ customer_service/src/main/java/com/duft/customer_service/Utils/RedisConfig/RedisUtil.java
@@ -2,6 +2,7 @@ package com.duft.customer_service.Utils.RedisConfig;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import tools.jackson.databind.ObjectMapper;
 
 public class RedisUtil {
