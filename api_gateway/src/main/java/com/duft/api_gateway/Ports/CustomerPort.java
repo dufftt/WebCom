666b653d350a65e6814b04252140f67607656cc9
Sourav <rockythere81@gmail.com>
2026-03-30 16:40:26 +0530

    backend is working now(both rest and graphql) gateway

--- api_gateway/src/main/java/com/duft/api_gateway/Ports/CustomerPort.java
+++
@@ -1,9 +0,0 @@
-package com.duft.api_gateway.Ports;
-
-import com.duft.api_gateway.Adapter.CustomerService.WebDTO.CustomerWebDTO;
-
-public interface CustomerPort {
-
-    CustomerWebDTO getCustomers();
-
-}
